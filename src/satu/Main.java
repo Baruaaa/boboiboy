5a6c065fb44fc900f2b5d8fae34503efed33554c
Baruaaa <akbarhernandi1212@gmail.com>
2024-11-01 13:53:29 +0700

    hai

---
+++ src/satu/Main.java
@@ -0,0 +1,28 @@
+package satu;
+
+import java.util.Scanner;
+
+class GanjilGenap {
+    public static void main(String[] args) {
+
+        Scanner input = getInput();
+
+
+        System.out.print("Masukkan angka: ");
+
+
+        if (input.nextInt() % 2 == 0) {
+            System.out.println(input.nextInt() + " adalah angka genap.");
+        } else {
+            System.out.println(input.nextInt() + " adalah angka ganjil.");
+        }
+
+
+        input.close();
+    }
+
+    private static Scanner getInput() {
+        Scanner input = new Scanner(System.in);
+        return input;
+    }
+}
